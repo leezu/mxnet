c944f927adc6226d8e745602b2e56746276e3eb2
Leonard Lausen <leonard@lausen.nl>
2018-05-11 22:13:41 +0000

    Check rows for division op

--- src/operator/sparse_dense_division-inl.h
+++ src/operator/sparse_dense_division-inl.h
@@ -85,13 +85,56 @@ inline bool SparseDenseDivisionOpStorageType(const nnvm::NodeAttrs &attrs,
   return dispatched;
 }
 
+template <typename xpu> struct sparse_div_lhs_rhs_index_map_kernel {
+  template <typename IType>
+  MSHADOW_XINLINE static void
+  Map(int i, const IType lhs_num_indices, const IType rhs_num_indices,
+      IType *rhs_idx_lhs_idx_map, const IType *lhs_index,
+      const IType *rhs_index) {
+    index_t lhs_pointer = 0;
+    index_t rhs_pointer = 0;
+    while (rhs_pointer < rhs_num_indices && lhs_pointer < lhs_num_indices) {
+      if (lhs_index[lhs_pointer] == rhs_index[rhs_pointer]) {
+        // inputs[0] and inputs[1] are aligned here
+        rhs_idx_lhs_idx_map[rhs_pointer] = lhs_pointer;
+        lhs_pointer++;
+        rhs_pointer++;
+        continue;
+      } else if (lhs_index[lhs_pointer] < rhs_index[rhs_pointer]) {
+        // inputs[0] has an element missing in inputs[1]
+        lhs_pointer++;
+        continue;
+      } else {
+        // inputs[1] has an element missing in inputs[0]
+        // which is unsupported
+        break; // TODO report error
+      }
+    }
+
+    // Check that we didn't exit prematurely due to running out of indices on
+    // lhs
+    if (rhs_pointer < rhs_num_indices) {
+      // Exited loopprematurely: Ran out of indices on lhs
+      // TODO report error. For now write invalid entries, that should force a
+      // crash
+      for (index_t rhs_pointer = 0; rhs_pointer < rhs_num_indices;
+           rhs_pointer++) {
+        rhs_idx_lhs_idx_map[rhs_pointer] = -1;
+      }
+    }
+  }
+};
+
 template <int req, typename xpu> struct sparse_div_kernel {
-  template <typename DType>
-  MSHADOW_XINLINE static void Map(int i, const index_t row_length, DType *out,
-                                  const DType *lhs, const DType *rhs) {
-    for (index_t j = 0; j < row_length; j++) {
-      KERNEL_ASSIGN(out[i * row_length + j], req,
-                    lhs[i * row_length + j] / rhs[i]);
+  template <typename DType, typename IType>
+  MSHADOW_XINLINE static void Map(int i, const IType row_length, DType *out,
+                                  const DType *lhs, const DType *rhs,
+                                  const IType *rhs_idx_lhs_idx_map) {
+    for (IType j = 0; j < row_length; j++) {
+      // TODO(leezu): This leaves empty rows in out if not KWriteTo and rhs row
+      // is missing
+      KERNEL_ASSIGN(out[rhs_idx_lhs_idx_map[i] * row_length + j], req,
+                    lhs[rhs_idx_lhs_idx_map[i] * row_length + j] / rhs[i]);
     }
   }
 };
@@ -121,14 +164,15 @@ void SparseDenseDivisionOpForwardEx(const nnvm::NodeAttrs &attrs,
   }
 
   if (rhs_stype == kRowSparseStorage) {
-    CHECK(inputs[0].storage_shape()[0] == inputs[1].storage_shape()[0])
-        << "lhs and rhs must have same number of rows.";
+    // CHECK(inputs[0].storage_shape()[0] == inputs[1].storage_shape()[0])
+    //     << "lhs and rhs must have same number of rows.";
     CHECK(inputs[1].storage_shape()[1] == 1)
         << "rhs array should have shape (X, 1)";
-  } else if (rhs_stype == kDefaultStorage) {
-    CHECK(inputs[0].storage_shape()[0] == inputs[1].shape()[0])
-        << "lhs and rhs must have same number of rows.";
-    CHECK(inputs[1].shape()[1] == 1) << "rhs array should have shape (X, 1)";
+    // } else if (rhs_stype == kDefaultStorage) {
+    //   CHECK(inputs[0].storage_shape()[0] == inputs[1].shape()[0])
+    //       << "lhs and rhs must have same number of rows.";
+    //   CHECK(inputs[1].shape()[1] == 1) << "rhs array should have shape (X,
+    //   1)";
   } else {
     LogUnimplementedOp(attrs, ctx, inputs, req, outputs);
   }
@@ -136,29 +180,45 @@ void SparseDenseDivisionOpForwardEx(const nnvm::NodeAttrs &attrs,
   CHECK_EQ(inputs[0].data().type_flag_, inputs[1].data().type_flag_);
   CHECK_EQ(inputs[0].data().type_flag_, outputs[0].data().type_flag_);
 
-  MSHADOW_TYPE_SWITCH(inputs[0].data().type_flag_, DType, {
-    MXNET_ASSIGN_REQ_SWITCH(req[0], req_type, {
-      if (req_type == kWriteTo) {
-        const nnvm::dim_t num_indices = inputs[0].storage_shape()[0];
-        outputs[0].CheckAndAlloc({mshadow::Shape1(num_indices)});
-        MSHADOW_IDX_TYPE_SWITCH(outputs[0].aux_type(rowsparse::kIdx), CType, {
-          mshadow::Copy(
-              outputs[0].aux_data(rowsparse::kIdx).FlatTo1D<xpu, CType>(),
-              inputs[0].aux_data(rowsparse::kIdx).FlatTo1D<xpu, CType>(), s);
-        });
-      }
-      const nnvm::dim_t num_rows = inputs[0].storage_shape()[0];
-      const nnvm::dim_t row_length = inputs[0].storage_shape()[1];
+  MSHADOW_IDX_TYPE_SWITCH(inputs[0].aux_type(rowsparse::kIdx), IType, {
+    const nnvm::dim_t lhs_num_indices = inputs[0].storage_shape()[0];
+    const nnvm::dim_t rhs_num_indices = inputs[1].storage_shape()[0];
+    mshadow::Tensor<xpu, 1, IType> rhs_idx_lhs_idx_map =
+        ctx.requested[0].get_space_typed<xpu, 1, IType>(
+            mshadow::Shape1(rhs_num_indices), s);
+    IType *rhs_idx_lhs_idx_map_value = rhs_idx_lhs_idx_map.dptr_;
+    const IType *lhs_index = inputs[0].aux_data(rowsparse::kIdx).dptr<IType>();
+    const IType *rhs_index = inputs[1].aux_data(rowsparse::kIdx).dptr<IType>();
 
-      const DType *lhs_value = inputs[0].data().dptr<DType>();
-      const DType *rhs_value = inputs[1].data().dptr<DType>();
-      DType *out_value = outputs[0].data().dptr<DType>();
+    Kernel<sparse_div_lhs_rhs_index_map_kernel<xpu>, xpu>::Launch(
+        s, 1, lhs_num_indices, rhs_num_indices, rhs_idx_lhs_idx_map_value,
+        lhs_index, rhs_index);
 
-      Kernel<sparse_div_kernel<req_type, xpu>, xpu>::Launch(
-          s, num_rows, row_length, out_value, lhs_value, rhs_value);
+    MSHADOW_TYPE_SWITCH(inputs[0].data().type_flag_, DType, {
+      MXNET_ASSIGN_REQ_SWITCH(req[0], req_type, {
+        if (req_type == kWriteTo) {
+          const nnvm::dim_t num_indices = inputs[0].storage_shape()[0];
+          outputs[0].CheckAndAlloc({mshadow::Shape1(num_indices)});
+          MSHADOW_IDX_TYPE_SWITCH(outputs[0].aux_type(rowsparse::kIdx), CType, {
+            mshadow::Copy(
+                outputs[0].aux_data(rowsparse::kIdx).FlatTo1D<xpu, CType>(),
+                inputs[0].aux_data(rowsparse::kIdx).FlatTo1D<xpu, CType>(), s);
+          });
+        }
+        const IType num_rows = inputs[1].storage_shape()[0];
+        const IType row_length = inputs[0].storage_shape()[1];
+
+        const DType *lhs_value = inputs[0].data().dptr<DType>();
+        const DType *rhs_value = inputs[1].data().dptr<DType>();
+        DType *out_value = outputs[0].data().dptr<DType>();
+
+        Kernel<sparse_div_kernel<req_type, xpu>, xpu>::Launch(
+            s, num_rows, row_length, out_value, lhs_value, rhs_value,
+            rhs_idx_lhs_idx_map_value);
+      });
     });
   });
-}
+} // namespace op
 } // namespace op
 } // namespace mxnet
 
